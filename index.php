8284ac6f6f5ff2e24be64978c4073e2a7e33a86d
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-10-19 15:21:22 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -6,4 +6,15 @@
 <?php require_once("header.php")?>
 
 
-Hub
+<div>
+    <h1>
+        Check out:
+    </h1>
+
+    <a class="btn btn-primary" href="dataOne.php"  role="button"> Baskets </a> 
+    <a class="btn btn-secondary" href="dataTwo.php"  role="button"> Drinks </a> 
+    <a class="btn btn-success" href="dataThree.php"  role="button"> Food </a> 
+    <a class="btn btn-info" href="dataFour.php"  role="button"> Stores </a> 
+</div>
+
+
