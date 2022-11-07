b63798db4c9a05f8e0bb69808aff12014f85d155
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-11-07 14:25:57 -0600

    Add files via upload

--- dataTwo.php
+++ dataTwo.php
@@ -24,18 +24,41 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle">Add Data</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+        </div>
+        <div class="modal-body">
+        
+
+
+            <div>
+                <form action="dataTwoAddPost.php" method="post">
+
+                    <lable for="drinkName" class="form-lable"> Drink Name </lable>
+                    <input type="text" name="drinkName" aria-describedby="Food Name">
+                    
+                    <lable for="drinkCost" class="form-lable"> Drink Cost </lable>
+                    <input type="text" name="drinkCost" aria-describedby="5.00">
+
+                    <lable for="drinkTemperature" class="form-lable"> Drink Temperature </lable>
+                    <input type="text" name="drinkTemperature" aria-describedby="Carb">
+
+                    <input type="submit">
+
+                </form>
+            </div>
+
+
+        </div>
+
+        <!-- <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal"> SUBMIT ADDITIONS</button>
+        </div> -->
+
+
+
     </div>
   </div>
 </div>
