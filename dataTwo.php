f2854b3f62d3deafebe50b93ebebee1ac19f6977
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-11-07 14:37:28 -0600

    Add files via upload

--- dataTwo.php
+++ dataTwo.php
@@ -6,17 +6,19 @@
 <?php require_once("header.php")?>
 
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-
 <div>
     <h1> Database 2 -Drinks Table</h1>
 </div>
 
 </br>
 
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+}) 
+
 <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-  Launch demo modal
+  Add
 </button>
 
 <!-- Modal -->
